306f0810fb154eebe109a44d50d130b28385de62
xackermonkeay <toxa_s000@mail.ru>
2023-02-05 01:28:41 +0300

    fix: добавлен домен

--- domain.js
+++ domain.js
@@ -1,2 +1,3 @@
 // export const domain = "http://172.20.10.4:8000/api"
-export const domain = "http://192.168.0.102:8000/api"
+// export const domain = "http://192.168.0.102:8000/api"
+export const domain = "http://http://chistiymir.pythonanywhere.com/api"
